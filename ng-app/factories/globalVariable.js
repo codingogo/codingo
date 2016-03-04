ac5901b24e402932bd3d120c752fcc8198199c06
Ji Lee <gafrong@gmail.com>
2016-03-04 22:54:00 +1100

    registration added

--- ng-app/factories/globalVariable.js
+++ ng-app/factories/globalVariable.js
@@ -1,5 +1,11 @@
 app.factory('globalVariable', function(){
   return {
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    hideModal: function (selector) {
+      $(selector).modal('hide')
+    },
+    showModal: function (selector) {
+      $(selector).modal('show')
+    }  
   }
 });
