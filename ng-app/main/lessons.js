7da16e1654d46c00f0ac1cd0826b6b5674115e4b
Ji Lee <gafrong@gmail.com>
2016-03-20 12:01:35 +1100

    video prev next button conditioned to user status

--- ng-app/main/lessons.js
+++ ng-app/main/lessons.js
@@ -9,7 +9,7 @@ app.controller('LessonsCtrl', function($scope, $rootScope, $stamplay, Lesson, Us
   var getUser = function(){
     UserStatus.getUser()
     .then(function(res){
-      console.log('user', res);
+      // console.log('user', res);
     })
   };
 
@@ -37,23 +37,62 @@ app.controller('LessonCtrl', function($scope, $stateParams, Lesson, Video, $sce)
     $scope.currentTabIndex = tabIndex;
     $scope.videoObj = $scope.filteredVideos[tabIndex];
     $scope.videoLink = [{url: $sce.trustAsResourceUrl('//fast.wistia.net/embed/iframe/' + $scope.videoObj.wistia)}];
+
+    window._wq = window._wq || [];
+    _wq.push({ "_all": function(video) {
+      video.play();
+    }});
+
+    // video next and previous buttons
+    if ($scope.filteredVideos[tabIndex + 1] === undefined){
+      $scope.allowNextBtn = false;
+    } else {
+      $scope.allowNextBtn = true;
+    }
+    if($scope.filteredVideos[tabIndex - 1] === undefined){
+      $scope.allowPreviousBtn = false;
+    } else {
+      $scope.allowPreviousBtn = true;
+    }
+  };
+
+  $scope.nextVideo = function(index){
+    if($scope.filteredVideos[index + 1] !== undefined){
+      $scope.videoObj = $scope.filteredVideos[index + 1];
+    } 
+    if ($scope.filteredVideos[index + 2] === undefined){
+      $scope.allowNextBtn = false;
+    }
+    if($scope.videoLink !== undefined){
+      $scope.videoLink = [{url: $sce.trustAsResourceUrl('//fast.wistia.net/embed/iframe/' + $scope.videoObj.wistia)}];
+      $scope.allowPreviousBtn = true;
+    }
+  };
+
+  $scope.previousVideo = function(index){
+    $scope.allowNextBtn = true;
+    $scope.videoObj = $scope.filteredVideos[index - 1];
+    if($scope.videoObj !== undefined){
+      $scope.videoLink = [{url: $sce.trustAsResourceUrl('//fast.wistia.net/embed/iframe/' + $scope.videoObj.wistia)}];
+      $scope.allowPreviousBtn = true;
+    };
+    if($scope.filteredVideos[index - 2] === undefined){
+      $scope.allowPreviousBtn = false;
+    }  
   };
 
   var initialise = function(){
     $scope.currentTabIndex = 0;
     $scope.lessonId = $stateParams.lessonId;
     $scope.videoId = $stateParams;
+    $scope.allowNextBtn = true;
+    $scope.allowPreviousBtn = false;
   
     Lesson.get($stateParams.lessonId)
       .then(function(data){
         $scope.premium = data.data[0].premium;
         $scope.lessonObj = data.data[0];
     });
-
-    if($scope.filteredVideos){
-      $scope.videoObj = $scope.filteredVideos[0];
-      console.log($scope.videoObj);
-    };
 
     Video.get($stateParams)
       .then(function(data){
