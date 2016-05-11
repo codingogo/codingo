237a7957eb05aaf3901f33ea689b84542fa5f516
ji <gafrong@gmail.com>
2016-05-12 09:23:23 +1000

    main listing page with new background styling

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var es = require('event-stream');
 
-gulp.task('scripts', function(){
+gulp.task('default', function(){
   gulp.src('src/*.js')
   .pipe(concat('all.min.js'))
   .pipe(uglify())
