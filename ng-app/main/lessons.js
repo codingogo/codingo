d5bf7ed82c03cfc045112c96e2f7985a202f0bff
ji <gafrong@gmail.com>
2016-06-14 17:20:08 +1000

    footer update

--- ng-app/main/lessons.js
+++ ng-app/main/lessons.js
@@ -2,7 +2,6 @@ module.exports= function(app){
   app.controller('LessonsCtrl', function($scope, $rootScope, $stamplay, Lesson, UserStatus, AnchorSmoothScroll, $location){
     $scope.lessons = [];
     $scope.footer = true;
-   
 
     var initialise = function(){
       loadLessons();
